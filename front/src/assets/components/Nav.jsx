56426d4881b78c817ee32a961ff72ad2098d6163
Clinton Nogueira Silva <clintonnogueirasilva@gmail.com>
2025-09-04 22:11:44 -0300

    Inicio

---
+++ front/src/assets/components/Nav.jsx
@@ -0,0 +1,16 @@
+function NavBar() {
+  
+  return (
+    <>
+      <nav>
+        <h1>Simpatia</h1>
+        <div className="links">
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default NavBar
