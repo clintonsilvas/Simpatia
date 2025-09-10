226135ee55f765249edf14c4e894936b55a8cfd3
rafaelodeclopes <rafaeloclopes2509@gmail.com>
2025-09-10 16:18:16 -0300

    Prévia do Footer

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,15 @@
 import Hero from './assets/pages/Hero.jsx'
+import React from 'react';
+import './index.css';
+import ButtonUnifenas from './assets/components/ButtonUnifenas.jsx';
+import Footer from './assets/components/Footer';
+
 function App() {
 
   return (
     <>
       <Hero />
+      <Footer />
     </>
   )
 }
---
+++ front/src/assets/components/Footer.jsx
@@ -0,0 +1,55 @@
+// Footer.jsx
+import React from 'react';
+import './Footer.css';
+import ButtonUnifenas from './ButtonUnifenas';
+import ButtonConhecerModulos from './ButtonConhecerModulos';
+
+function Footer() {
+  return (
+    <footer className="footer-container">
+      <div className="footer-content">
+        {/* Divisão Esquerda */}
+        <div className="footer-left">
+          <img src="/logo-simpatia.svg" alt="Logo Simpatia" className="footer-logo-simpatia" />
+          <div className="footer-text-box">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio expedita optio impedit suscipit quos illo sint ea sed? Recusandae itaque voluptate ex doloremque autem harum necessitatibus nesciunt aliquam, maiores pariatur!
+            </p>
+          </div>
+          <ButtonConhecerModulos/>
+        </div>
+
+        {/* Divisão Meio/Direita */}
+        <div className="footer-right">
+          {/* Divisão de cima */}
+          <div className="footer-right-top">
+            {/* Coluna 1 (links) */}
+            <div className="footer-column">
+              <h3>Para Professores</h3>
+              <ul>
+                <li><a href="#sobre">Gerador de Plano de Aulas</a></li>
+                <li><a href="#cursos">Corretor de Questões Descritivas</a></li>
+              </ul>
+            </div>
+            {/* Coluna 2 (links) */}
+            <div className="footer-column">
+              <h3>Para Alunos</h3>
+              <ul>
+                <li><a href="#blog">Simpático</a></li>
+              </ul>
+            </div>
+          </div>
+          {/* Divisão de baixo */}
+          <div className="footer-right-bottom">
+            <div className="footer-text-know-more">
+              Conheça mais sobre o projeto
+            </div>
+            <ButtonUnifenas></ButtonUnifenas>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
