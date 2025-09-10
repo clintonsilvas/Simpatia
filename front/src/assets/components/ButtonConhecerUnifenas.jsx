f0c810e7d0b49638545b50b0f74d054283bc44be
rafaelodeclopes <rafaeloclopes2509@gmail.com>
2025-09-09 23:10:11 -0300

    BRANCH CERTA - Teste de Footer e prévia do botão "Conheça a Unifenas" (Correção de missclick)

---
+++ front/src/assets/components/ButtonConhecerUnifenas.jsx
@@ -0,0 +1,11 @@
+function ButtonConhecerUnifenas() {
+  return (
+    <button>
+      <img src="/unifenas-logo.svg" alt="Logo da Unifenas" />
+      Conheça a Unifenas
+    </button>
+  );
+
+}
+
+export default ButtonConhecerUnifenas
