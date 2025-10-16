825d345b433e7acf6996cba4258c7d42fefa28e4
rafaelodeclopes <rafaeloclopes2509@gmail.com>
2025-10-16 13:57:09 -0300

    Outras alterações relacionadas à página de descrição

--- front/src/UpdatesPage.jsx
+++ front/src/UpdatesPage.jsx
@@ -1,15 +1,37 @@
-import Hero from './assets/pages/Hero.jsx';
+import HeroAlt from './assets/pages/HeroAlt.jsx';
 import Faq from './assets/pages/Faq.jsx';
 import Footer from './assets/components/Footer.jsx';
 import UnifenasAlt from './assets/pages/UnifenasAlt.jsx';
 import Atualizacoes from './assets/components/Atualizacoes.jsx';
 function UpdatesPage() {
 
+  const linhaDivisoria = {
+    width: '100%',
+    height: '0',
+    margin: '64px 0',
+    borderTop: '2px dashed var(--roxo-claro-03)',
+  };
+
   return (
     <>
-      <Hero/>
+      <HeroAlt
+      tagDeContexto="ATUALIZAÇÕES"
+      tituloPrincipal={
+      <>
+        Conheça mais <br />
+        <span className="texto_azul">sobre o Projeto</span>
+      </>
+      } 
+      subtitulo={
+      <>
+        Ferramentas criadas para apoiar alunos e professores <br />
+        na sala de aula.
+      </>
+      } 
+    />
       <UnifenasAlt />
       <Atualizacoes />
+      <div style={linhaDivisoria}></div>
       <Faq />
       <Footer/>     
     </>
---
+++ front/src/assets/pages/HeroAlt.jsx
@@ -0,0 +1,29 @@
+import NavBar from "../components/Nav";
+import "./Hero.css";
+
+function HeroAlt({ tagDeContexto, tituloPrincipal, subtitulo }) {
+ return (
+  <section className="fundo-hero">
+   <NavBar />
+   <div className="secao-hero-alt">
+    <div className="hero-container-alt">
+     
+     {tagDeContexto && (
+      <div className="tag-contexto-hero">
+       {tagDeContexto}
+      </div>
+     )}
+
+     <h1>
+      {tituloPrincipal}
+     </h1>
+     <p>
+      {subtitulo}
+     </p>
+    </div>
+   </div>
+  </section>
+ );
+}
+
+export default HeroAlt;
