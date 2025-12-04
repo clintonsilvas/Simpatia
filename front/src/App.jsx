f5a33f7dfa157341881ccb6d049f18f2a2e10cfa
Clinton Nogueira Silva <clintonnogueirasilva@gmail.com>
2025-12-04 20:17:56 -0300

    Ajuste about

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,20 +1,15 @@
-import Hero from './assets/pages/Hero.jsx';
-import Faq from './assets/pages/Faq.jsx';
-import Footer from './assets/components/Footer.jsx';
-import Unifenas from './assets/pages/Unifenas.jsx';
-import Descubra from './assets/pages/Descubra.jsx';
+import Hero from "./assets/pages/Hero.jsx";
+import Faq from "./assets/pages/Faq.jsx";
+import Footer from "./assets/components/Footer.jsx";
+import Unifenas from "./assets/pages/Unifenas.jsx";
+import Descubra from "./assets/pages/Descubra.jsx";
+import UpdatesPage from "./UpdatesPage.jsx";
 function App() {
-
   return (
     <>
-      <Hero/>
-      <Descubra />
-      <Unifenas />
-      <Faq />
-      <Footer/>     
-      
+      <UpdatesPage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
